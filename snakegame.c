afb2a45b25216f241f66bf897272bcd49d1a76a4
Beomsu <310o@naver.com>
2020-04-15 09:59:01 +0900

    food(),result of game 결과과

--- snakegame.c
+++ snakegame.c
@@ -21,7 +21,7 @@ int x[100], y[100]; //x,y 좌표값을 저장 총 100개
 int length; //몸길이를 기억 
 int speed; //게임 속도 
 int score; //점수 저장  --reset함수에 의해 초기화됨
-
+int food_x, food_y;
 int dir; //이동방향 저장 
 int key; //입력받은 키 저장 
 
@@ -31,7 +31,7 @@ void gotoxy(int x, int y,const char* s) {
     printf("%s", s);
 }
 
-
+void food();
 void reset(void); //게임을 초기화 
 void draw_map();
 void move(int dir); //뱀머리를 이동 
@@ -66,14 +66,15 @@ int main() {
 void draw_map(void) { 
     int i;
     for (i = 0; i < MAP_WIDTH; i++) {
-        gotoxy(i, 0, "*");
-        gotoxy(i, MAP_HEIGHT, "*");
+        gotoxy(MAP_X + i, MAP_Y, "*");
     }
-    for (i = 0 ; i < MAP_HEIGHT; i++) {
-        gotoxy(0,i, "*");
-        gotoxy(MAP_WIDTH,i, "*");
+    for (i = MAP_Y + 1; i < MAP_Y + MAP_HEIGHT - 1; i++) {
+        gotoxy(MAP_X, i, "*");
+        gotoxy(MAP_X + MAP_WIDTH - 1, i, "*");
     }
-\
+    for (i = 0; i < MAP_WIDTH; i++) {
+        gotoxy(MAP_X + i, MAP_Y + MAP_HEIGHT - 1, "*");
+    }
 }
 
 void reset(void) {
@@ -93,11 +94,39 @@ void reset(void) {
     }
     gotoxy(MAP_X + x[0], MAP_Y + y[0], "e");  
 
-
+    food();
 }
+void game_over(void) { //게임종료 함수 
+    gotoxy(MAP_X + (MAP_WIDTH / 2) - 6, MAP_Y + 5, "+----------------------+");
+    gotoxy(MAP_X + (MAP_WIDTH / 2) - 6, MAP_Y + 6, "|      GAME OVER..     |");
+    gotoxy(MAP_X + (MAP_WIDTH / 2) - 6, MAP_Y + 7, "+----------------------+");
+    gotoxy(MAP_X + (MAP_WIDTH / 2) - 6, MAP_Y + 8, " YOUR SCORE : ");
+ 
 
+
+
+    Sleep(500);
+
+  
+}
 void move(int dir) {
     int i;
+
+    if (x[0] == food_x && y[0] == food_y) { //food와 충돌했을 경우 
+        score += 10; //점수 증가 
+        food(); //새로운 food 추가 
+        length++; //길이증가 
+        x[length - 1] = x[length - 2]; //새로만든 몸통에 값 입력 
+        y[length - 1] = y[length - 2];
+    }
+
+    for (i = 1; i < length; i++) { //자기몸과 충돌했는지 검사 
+        if (x[0] == x[i] && y[0] == y[i]) {
+            game_over();
+            return;
+        }
+    }
+
 
     gotoxy(MAP_X + x[length - 1], MAP_Y + y[length - 1], "  "); 
     for (i = length - 1; i > 0; i--) { 
@@ -110,8 +139,42 @@ void move(int dir) {
     if (dir == UP) --y[0];
     if (dir == DOWN) ++y[0];
     gotoxy(MAP_X + x[i], MAP_Y + y[i], "e"); 
+    
+    if (x[0] == 0 || x[0] == MAP_WIDTH - 1 || y[0] == 0 || y[0] == MAP_HEIGHT - 1) { //벽과 충돌했을 경우 
+        game_over();
+        return;
+    }
 }
 
 
 
+void food(void) {
+    int i;
 
+    int food_crush_on = 0;//food가 뱀 몸통좌표에 생길 경우 on 
+    int r = 0; //난수 생성에 사동되는 변수 
+    gotoxy(MAP_X, MAP_Y + MAP_HEIGHT, " YOUR SCORE: "); //점수표시 
+
+
+    while (1) {
+        food_crush_on = 0;
+        srand((unsigned)time(NULL) + r); //난수표생성 
+        food_x = (rand() % (MAP_WIDTH - 2)) + 1;    //난수를 좌표값에 넣음 
+        food_y = (rand() % (MAP_HEIGHT - 2)) + 1;
+
+        for (i = 0; i < length; i++) { //food가 뱀 몸통과 겹치는지 확인  
+            if (food_x == x[i] && food_y == y[i]) {
+                food_crush_on = 1; //겹치면 food_crush_on 를 on 
+                r++;
+                break;
+            }
+        }
+
+        if (food_crush_on == 1) continue; //겹쳤을 경우 while문을 다시 시작 
+
+        gotoxy(MAP_X + food_x, MAP_Y + food_y, "♪"); //안겹쳤을 경우 좌표값에 food를 찍고 
+        speed -= 3; //속도 증가 
+        break;
+
+    }
+}
