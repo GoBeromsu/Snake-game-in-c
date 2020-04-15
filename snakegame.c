87c9d00ba5113a7e7a8726b52e7ca7c083535664
hewin0229 <63284344+hewin0229@users.noreply.github.com>
2020-04-15 17:50:52 +0900

    Update snakegame.c

--- snakegame.c
+++ snakegame.c
@@ -67,14 +67,14 @@ int main() {
 void draw_map(void) { 
     int i;
     for (i = 0; i < MAP_WIDTH; i++) {
-        gotoxy(MAP_X + i, MAP_Y, "*");
+        gotoxy(MAP_X + i, MAP_Y, "■");
     }
     for (i = MAP_Y + 1; i < MAP_Y + MAP_HEIGHT - 1; i++) {
-        gotoxy(MAP_X, i, "*");
-        gotoxy(MAP_X + MAP_WIDTH - 1, i, "*");
+        gotoxy(MAP_X, i, "■");
+        gotoxy(MAP_X + MAP_WIDTH - 1, i, "■");
     }
     for (i = 0; i < MAP_WIDTH; i++) {
-        gotoxy(MAP_X + i, MAP_Y + MAP_HEIGHT - 1, "*");
+        gotoxy(MAP_X + i, MAP_Y + MAP_HEIGHT - 1, "■");
     }
 }
 
